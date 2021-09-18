ae8d732c195beecb840d24a7dac76cd47c369305
Victor Moreno Jiménez <victor.moreno@connectedhealthservices.com>
2021-09-19 01:32:25 +0200

    Change name

--- pipelines/example-shared-library.groovy
+++ pipelines/example-shared-library.groovy
@@ -1,6 +1,6 @@
 @Library('chs-basic-shared-library') _
 
-String repository = params.GITHUB_PR_HEAD_SHA
+String repository = params.REPOSITORY
 String reference = params.getOrDefault('REFERENCE', 'main')
 
 pipeline {

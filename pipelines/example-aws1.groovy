69d86c2d743c08eb188afd8bc1d2439d5153915b
Víctor Moreno Jiménez <victormoreno.tech@protonmail.com>
2021-09-18 10:10:18 +0200

    Create example-aws1.groovy

---
+++ pipelines/example-aws1.groovy
@@ -0,0 +1,16 @@
+@Library('github.com/releaseworks/jenkinslib') _
+
+pipeline {
+  agent {
+    label 'ecs'
+  }
+  stages {
+    stage('AWS test Stage') {
+      steps {
+        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'aws-integration-ohio-key', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+          AWS("--region=us-east-2 s3 ls")
+        }
+      }
+    }
+  }
+}

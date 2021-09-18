2c165e6d887cf85a2c1b0584d4b61d6235ed0e2b
Víctor Moreno Jiménez <victormoreno.tech@protonmail.com>
2021-09-18 11:11:33 +0200

    Update example-aws1.groovy

--- pipelines/example-aws1.groovy
+++ pipelines/example-aws1.groovy
@@ -7,9 +7,7 @@ pipeline {
   stages {
     stage('AWS test Stage') {
       steps {
-        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'aws-integration-ohio-key', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-          AWS("--region=us-east-2 s3 ls")
-        }
+        sh 'aws --region us-east-2 s3 ls'
       }
     }
   }

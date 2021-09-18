55e67f23c3cc71b40337c31618e5e357a734b329
Victor Moreno Jiménez <victor.moreno@connectedhealthservices.com>
2021-09-19 01:45:08 +0200

    fix error in path

--- jenkins-shared-library/vars/gitClone.groovy
+++ jenkins-shared-library/vars/gitClone.groovy
@@ -2,7 +2,7 @@ def call(String repository, String reference){
   stage("Git clone repository ${repository}"){
     script {
       git branch: reference, url: repository
-      sh 'bash /src/script.sh'
+      sh 'bash ./src/script.sh'
     }
   }
 }

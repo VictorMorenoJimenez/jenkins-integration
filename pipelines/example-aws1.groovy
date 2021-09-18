002b71e0e9b358c0224dae5f02903aa07081ce24
Víctor Moreno Jiménez <victormoreno.tech@protonmail.com>
2021-09-19 00:36:18 +0200

    Update example-aws1.groovy

--- pipelines/example-aws1.groovy
+++ pipelines/example-aws1.groovy
@@ -4,6 +4,10 @@ pipeline {
   agent {
     label 'ecs'
   }
+  environment {
+    AWS_ACCESS_KEY_ID     = credentials('aws-key-id-ohio')
+    AWS_SECRET_ACCESS_KEY = credentials('	aws-secret-key-ohio')
+  }
   stages {
     stage('AWS test Stage') {
       steps {

629fd055ba1e5254af1db81ccb60ab9f4c434bd9
Victor Moreno Jiménez <victor.moreno@connectedhealthservices.com>
2021-09-19 16:46:09 +0200

    Add xterm and new folders

---
+++ pipelines/example2.groovy
@@ -0,0 +1,23 @@
+@Library('chs-basic-shared-library') _
+
+String repository = params.REPOSITORY
+String reference = params.getOrDefault('REFERENCE', 'main')
+
+pipeline {
+  agent {
+    label 'ecs'
+  }
+  stages {
+    stage('Clone repository') {
+      steps {
+        git url: repository, branch: reference
+      }
+    }
+
+    stage('Execute bash command') {
+      steps {
+        println(sh(script: 'ls -larth', returnStdout: true))
+      }
+    }
+  }
+}

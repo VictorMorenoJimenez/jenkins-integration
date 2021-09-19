a0874c5ddd868dee21a0bf2213623909e7d97459
Victor Moreno Jiménez <victor.moreno@connectedhealthservices.com>
2021-09-19 17:27:51 +0200

    Add pipeline to destroy stacks

---
+++ pipelines/cdk/example-destroy-cdk.groovy
@@ -0,0 +1,37 @@
+@Library ('chs-basic-shared-library') _
+
+import org.chs.Git
+import org.chs.CdkCli
+import org.chs.NpmCli
+
+node('ecs-node'){
+  ansiColor('xterm'){
+    String cdkRepository = params.CDK_REPOSITORY
+    String cdkReference = params.CDK_REFERENCE
+    def awsCli = new AwsCli(this)
+    def gitCli = new Git(this)
+    def cdkCli = new CdkCli(this)
+    def npmCli = new NpmCli(this)
+
+    stage('Clone repository') {
+      gitCli.checkout(cdkRepository, cdkReference)
+    }
+
+    stage('Install CDK project dependencies') {
+      println(npmCli.install())
+    }
+
+    stage('Launch CDK tests') {
+      println(npmCli.test())
+    }
+
+    stage('Build CDK project') {
+      println(npmCli.build())
+    }
+
+    stage('Destroy stacks') {
+      println(cdkCli.destroy())
+    }
+  }
+}
+

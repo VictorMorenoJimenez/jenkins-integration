8a66e5622833a88b62a528fa1ec0e29e3d8f1bfa
Victor Moreno Jiménez <victor.moreno@connectedhealthservices.com>
2021-09-19 17:08:14 +0200

    Fix error in npmcli

--- jenkins-shared-library/src/org/chs/NpmCli.groovy
+++ jenkins-shared-library/src/org/chs/NpmCli.groovy
@@ -9,16 +9,16 @@ class NpmCli implements Serializable {
   }
 
   def install(String registry = 'https://registry.npmjs.org/') {
-    String configRegistryOutput = sh(script:"npm config set registry ${registry}", returnStdout: true)
-    String npmInstallOutput = sh(script: 'npm install', returnStdout: true)
+    String configRegistryOutput = this.pipelineScript.sh(script:"npm config set registry ${registry}", returnStdout: true)
+    String npmInstallOutput = this.pipelineScript.sh(script: 'npm install', returnStdout: true)
     return configRegistryOutput + '\n' + npmInstallOutput
   }
 
   def test() {
-    return sh(script: 'npm run test', returnStdout: true)
+    return this.pipelineScript.sh(script: 'npm run test', returnStdout: true)
   }
 
   def build() {
-    return sh(script: 'npm run build', returnStdout: true)
+    return this.pipelineScript.sh(script: 'npm run build', returnStdout: true)
   }
 }

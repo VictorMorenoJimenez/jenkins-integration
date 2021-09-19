04546f4ad17eb2fe04d7dabd0b548b58f0008bda
Victor Moreno Jiménez <victor.moreno@connectedhealthservices.com>
2021-09-19 14:49:38 +0200

    Add new pipeline

---
+++ pipelines/example-aws-node1.groovy
@@ -0,0 +1,16 @@
+@Library ('chs-basic-shared-library') _
+import org.chs.AwsCli
+import org.chs.Git
+
+
+node('ecs'){
+  String cdkRepository = params.CDK_REPOSITORY
+  String reference = params.REFERENCE
+  def awsCli = new AwsCli(this)
+  def gitCli = new Git(this)
+
+  stage('AWS shared library') {
+    gitCli.checkout(repository, reference)
+    sh(command: 'ls -larth', returnStdout: true)
+  }
+}

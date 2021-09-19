4a145c22bb29f0787c86c4c1df65441108e4aa0f
Victor Moreno Jiménez <victor.moreno@connectedhealthservices.com>
2021-09-19 10:38:26 +0200

    Change AWS name

--- jenkins-shared-library/src/org/chs/AwsCli.groovy
+++ jenkins-shared-library/src/org/chs/AwsCli.groovy
@@ -1,24 +1,21 @@
 package org.chs
 import org.chs.Constants
 
-class AWS implements Serializable {
-  
-  static final String BINARY = 'aws'
+class AwsCli implements Serializable {
   static final String REGION = 'us-east-2'
   static final int AWS_CALL_RETRIES = 3
   static final int AWS_CALL_RETRIES_WAIT = 5
 
   private final def pipelineScript
 
-  AWS(def pipelineScript) {
+  AwsCli(def pipelineScript) {
     this.pipelineScript = pipelineScript
   }
 
-  boolean finished = false
-  def executeCommand(List params, String region = AWS.REGION) {
+  def executeCommand(List params, String region = AwsCli.REGION) {
     String ACCESS_KEY_SECRET_ID = this.pipelineScript.credentials(Constants.JENKINS_AWS_CREDENTIALS_ACCESS_KEY_ID_ID)
     String SECRET_KEY_SECRET_ID = this.pipelineScript.credentials(Constants.JENKINS_AWS_SECRET_ACCESS_KEY_ID)
-    String command = "AWS_ACCESS_KEY_ID=${ACCESS_KEY_SECRET_ID} AWS_SECRET_ACCESS_KEY=${SECRET_KEY_SECRET_ID} ${AWS.BINARY} --region ${region} "
+    String command = "AWS_ACCESS_KEY_ID=${ACCESS_KEY_SECRET_ID} AWS_SECRET_ACCESS_KEY=${SECRET_KEY_SECRET_ID} aws --region ${region} "
     String output = ''
     boolean retry = false
 
@@ -29,11 +26,9 @@ class AWS implements Serializable {
 
     this.pipelineScript.sh(script: "echo ${command}")
 
-    println('Printing command ' + command)
-
-    this.pipelineScript.retry(AWS_CALL_RETRIES){
+    this.pipelineScript.retry(AwsCli.AWS_CALL_RETRIES){
       if(retry) {
-        this.pipelineScript.sleep(AWS_CALL_RETRIES_WAIT)
+        this.pipelineScript.sleep(AwsCli.AWS_CALL_RETRIES_WAIT)
       } else {
         retry = true
       }
--- pipelines/example-aws2.groovy
+++ pipelines/example-aws2.groovy
@@ -1,16 +1,16 @@
 @Library ('chs-basic-shared-library') _
-import org.chs.AWS
+import org.chs.AwsCli
 import org.chs.Git
 
 
 node('ecs'){
   String repository = 'https://github.com/VictorMorenoJimenez/jenkins-code-example.git'
   String reference = 'main'
-  def awsCli = new AWS(this)
+  def awsCli = new AwsCli(this)
   def gitCli = new Git(this)
 
   stage('AWS test Stage') {
-    gitCli.checkout(repository, reference)
-    //awsCli.executeCommand(['s3', 'ls'])
+    //gitCli.checkout(repository, reference)
+    awsCli.executeCommand(['s3', 'ls'])
   }
 }

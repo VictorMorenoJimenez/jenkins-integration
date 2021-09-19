50e78a741ab0471f62fa9ebcf7afd6627d143cd9
Victor Moreno Jiménez <victor.moreno@connectedhealthservices.com>
2021-09-19 14:56:49 +0200

    Fix error in pipeline

--- pipelines/example-aws-node1.groovy
+++ pipelines/example-aws-node1.groovy
@@ -5,12 +5,12 @@ import org.chs.Git
 
 node('ecs-node'){
   String cdkRepository = params.CDK_REPOSITORY
-  String reference = params.CDK_REFERENCE
+  String cdkReference = params.CDK_REFERENCE
   def awsCli = new AwsCli(this)
   def gitCli = new Git(this)
 
   stage('AWS shared library') {
-    gitCli.checkout(repository, reference)
+    gitCli.checkout(cdkRepository, cdkReference)
     sh(command: 'ls -larth', returnStdout: true)
   }
 }

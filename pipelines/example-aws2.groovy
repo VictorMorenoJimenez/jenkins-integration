ca2b2c20bf73ce152c987a540ac03090a6ff84c8
Victor Moreno Jiménez <victor.moreno@connectedhealthservices.com>
2021-09-19 10:20:11 +0200

    Add git to example

--- pipelines/example-aws2.groovy
+++ pipelines/example-aws2.groovy
@@ -1,12 +1,16 @@
 @Library ('chs-basic-shared-library') _
 import org.chs.AWS
+import org.chs.Git
 
 
 node('ecs'){
+  String repository = 'https://github.com/VictorMorenoJimenez/jenkins-code-example.git'
+  String reference = 'main'
   def awsCli = new AWS(this)
-  
+  def gitCli = new Git(this)
   stage('AWS test Stage') {
     steps {
+      gitCli.checkout(repository, reference)
       awsCli.executeCommand(['s3', 'ls'])
     }
   }

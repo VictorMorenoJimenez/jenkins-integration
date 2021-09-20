6783f12e128ff0f31817b118f58e697beb142e54
Victor Moreno Jiménez <victor.moreno@connectedhealthservices.com>
2021-09-20 21:20:22 +0200

    Remove steps

--- pipelines/basic/example3v2.groovy
+++ pipelines/basic/example3v2.groovy
@@ -2,22 +2,16 @@ node('ecs'){
   try {
     ansiColor('xterm'){
       stage('Create directory') {
-        steps {
-          sh(script: 'mkdir -p output')
-        }
+        sh(script: 'mkdir -p output')
       }
 
       stage('Create new file') {
-        steps {
-          writeFile file: "output/file.txt", text: "This file is my useful, we need to save it."
-          writeFile file: "output/useless-file.md", text: "This file is my useless, discard it."
-        }
+        writeFile file: "output/file.txt", text: "This file is my useful, we need to save it."
+        writeFile file: "output/useless-file.md", text: "This file is my useless, discard it."
       }
 
       stage('Archive my new file') {
-        steps {
-          archiveArtifacts artifacts: 'output/*.txt', excludes: 'output/*.md'
-        }
+        archiveArtifacts artifacts: 'output/*.txt', excludes: 'output/*.md'
       }
     }
   } finally {

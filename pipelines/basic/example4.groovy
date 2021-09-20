342894d06e7ef03b955125ff99659e5c83476440
Victor Moreno Jiménez <victor.moreno@connectedhealthservices.com>
2021-09-20 22:31:30 +0200

    Fix parallel:

--- pipelines/basic/example4.groovy
+++ pipelines/basic/example4.groovy
@@ -1,40 +1,54 @@
 node('ecs'){
   try {
-    parallel {
+    List stagesDir = []
+    stages.push({
       stage('Create directory 1') {
         sh(script: 'mkdir -p output')
-      }
+      }      
+    })
 
+    stages.push({
       stage('Create directory 2') {
         sh(script: 'mkdir -p output2')
       }
+    })
 
+    stages.push({
       stage('Create directory 3') {
         sh(script: 'mkdir -p output2')
-      }      
-    }
+      }  
+    })
 
-    parallel {
+    List stagesFiles = []
+
+    stagesFiles.push({
       stage('Create new file 1') {
         writeFile file: "output1/file.txt", text: "This file is my useful, we need to save it."
         writeFile file: "output1/useless-file.md", text: "This file is my useless, discard it."
       }
+    })
 
+    stagesFiles.push({
       stage('Create new file 2') {
         writeFile file: "output2/file.txt", text: "This file is my useful, we need to save it."
         writeFile file: "output2/useless-file.md", text: "This file is my useless, discard it."
       }
+    })
 
+    stagesFiles.push({
       stage('Create new file 3') {
         writeFile file: "output3/file.txt", text: "This file is my useful, we need to save it."
         writeFile file: "output3/useless-file.md", text: "This file is my useless, discard it."
       }
-    }
+    })
+    
+    parallel { stagesDir }
+    parallel { stagesFiles }
 
     stage('Archive my new file') {
       archiveArtifacts artifacts: 'output*/*.txt', excludes: 'output/*.md'
     }
-    
+
   } finally {
     cleanWs(cleanWhenFailure: false, cleanWhenUnstable: false)
   }

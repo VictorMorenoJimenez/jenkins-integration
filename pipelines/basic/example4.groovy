2b3d23ffeace6507ffac17e42d7d7c79d26ae877
Victor Moreno Jiménez <victor.moreno@connectedhealthservices.com>
2021-09-20 22:40:54 +0200

    Fix parallel

--- pipelines/basic/example4.groovy
+++ pipelines/basic/example4.groovy
@@ -1,46 +1,46 @@
 node('ecs'){
   try {
-    List stagesDir = []
-    stagesDir.push({
+    Map stagesDir = [:]
+    stagesDir['createDir1'] = {
       stage('Create directory 1') {
         sh(script: 'mkdir -p output')
       }      
-    })
+    }
 
-    stagesDir.push({
+    stagesDir['createDir2'] = {
       stage('Create directory 2') {
         sh(script: 'mkdir -p output2')
       }
-    })
+    }
 
-    stagesDir.push({
+    stagesDir['createDir3'] = {
       stage('Create directory 3') {
-        sh(script: 'mkdir -p output2')
-      }  
-    })
+        sh(script: 'mkdir -p output3')
+      }
+    }
 
-    List stagesFiles = []
+    Map stagesFiles = [:]
 
-    stagesFiles.push({
+    stagesFiles['newFile1'] = {
       stage('Create new file 1') {
         writeFile file: "output1/file.txt", text: "This file is my useful, we need to save it."
         writeFile file: "output1/useless-file.md", text: "This file is my useless, discard it."
       }
-    })
+    }
 
-    stagesFiles.push({
+    stagesFiles['newFile2'] = {
       stage('Create new file 2') {
         writeFile file: "output2/file.txt", text: "This file is my useful, we need to save it."
         writeFile file: "output2/useless-file.md", text: "This file is my useless, discard it."
       }
-    })
+    }
 
-    stagesFiles.push({
+    stagesFiles['newFile3'] = {
       stage('Create new file 3') {
         writeFile file: "output3/file.txt", text: "This file is my useful, we need to save it."
         writeFile file: "output3/useless-file.md", text: "This file is my useless, discard it."
       }
-    })
+    }
     
     parallel { stagesDir }
     parallel { stagesFiles }

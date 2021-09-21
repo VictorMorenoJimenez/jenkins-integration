bd29df25a3cde010c2c5772f0d4b7fd3da5fdae7
Victor Moreno Jiménez <victor.moreno@connectedhealthservices.com>
2021-09-21 07:51:24 +0200

    Use named arguments

--- pipelines/basic/example3v2.groovy
+++ pipelines/basic/example3v2.groovy
@@ -1,22 +1,20 @@
 node('ecs'){
   try {
-    ansiColor('xterm'){
-      stage('Create directory') {
-        sh(script: 'mkdir -p output')
-      }
+    stage('Create directory') {
+      sh(script: 'mkdir -p output')
+    }
 
-      stage('Create new file') {
-        writeFile file: "output/file.txt", text: "This file is my useful, we need to save it."
-        writeFile file: "output/useless-file.md", text: "This file is my useless, discard it."
-      }
+    stage('Create new file') {
+      writeFile file: "output/file.txt", text: "This file is my useful, we need to save it."
+      writeFile file: "output/useless-file.md", text: "This file is my useless, discard it."
+    }
 
-      stage('Archive my new file') {
-        archiveArtifacts artifacts: 'output/*.txt', excludes: 'output/*.md'
-      }
+    stage('Archive my new file') {
+      archiveArtifacts artifacts: 'output/*.txt', excludes: 'output/*.md'
+    }
 
-      stage('AnsiColor test') {
-        sh('ls -larth')
-      }
+    stage('AnsiColor test') {
+      sh('ls -larth')
     }
   } finally {
     cleanWs(cleanWhenFailure: false, cleanWhenUnstable: false)

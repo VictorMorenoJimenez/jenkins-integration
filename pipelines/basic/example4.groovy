34b32a47cba32911806a5525d284e2b0a196deb2
Victor Moreno Jiménez <victor.moreno@connectedhealthservices.com>
2021-09-24 02:44:07 +0200

    Update example 4

--- pipelines/basic/example4.groovy
+++ pipelines/basic/example4.groovy
@@ -1,57 +1,32 @@
-node('ecs'){
-  try {
-    Map stagesDir = [:]
-    stagesDir['createDir1'] = {
-      stage('Create directory 1') {
-        sh(script: 'mkdir -p output')
-      }      
-    }
-
-    stagesDir['createDir2'] = {
-      stage('Create directory 2') {
-        sh(script: 'mkdir -p output2')
+pipeline {
+  agent {
+    label 'ecs'
+  }
+  stages {
+    stage('AWS parallel Stage') {
+      parallel {
+        stage('Create directory 1') {
+          steps {
+            sh(script: 'mkdir -p output')
+          }
+        }
+        stage('Create directory 2') {
+          steps {
+            sh(script: 'mkdir -p output2')
+          }
+        }
+        stage('Create directory 3') {
+          steps {
+            sh(script: 'mkdir -p output3')
+          }
+        }    
       }
     }
-
-    stagesDir['createDir3'] = {
-      stage('Create directory 3') {
-        sh(script: 'mkdir -p output3')
+    post {
+      always {
+        cleanWs(cleanWhenFailure: false, cleanWhenUnstable: false)
       }
     }
-
-    Map stagesFiles = [:]
-
-    stagesFiles['newFile1'] = {
-      stage('Create new file 1') {
-        writeFile file: "output1/file.txt", text: "This file is my useful, we need to save it."
-        writeFile file: "output1/useless-file.md", text: "This file is my useless, discard it."
-      }
-    }
-
-    stagesFiles['newFile2'] = {
-      stage('Create new file 2') {
-        writeFile file: "output2/file.txt", text: "This file is my useful, we need to save it."
-        writeFile file: "output2/useless-file.md", text: "This file is my useless, discard it."
-      }
-    }
-
-    stagesFiles['newFile3'] = {
-      stage('Create new file 3') {
-        writeFile file: "output3/file.txt", text: "This file is my useful, we need to save it."
-        writeFile file: "output3/useless-file.md", text: "This file is my useless, discard it."
-      }
-    }
-    
-    parallel { stagesDir }
-    parallel { stagesFiles }
-
-    stage('Archive my new file') {
-      archiveArtifacts artifacts: 'output*/*.txt', excludes: 'output/*.md'
-    }
-
-  } finally {
-    cleanWs(cleanWhenFailure: false, cleanWhenUnstable: false)
   }
 }
-
 
